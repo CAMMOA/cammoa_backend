00019f4a9414e28a154ba6b26196577bbb42ab60
kim <202204266@hufs.ac.kr>
2025-04-29 22:06:10 +0900

    [refactor] 회원가입 닉네임 추가

--- src/main/java/org/example/users/dto/request/UserCreateRequest.java
+++ src/main/java/org/example/users/dto/request/UserCreateRequest.java
@@ -1,7 +1,6 @@
 package org.example.users.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
