73e53028405a8bb855a92cd7118e44eedb6960b0
kim <202204266@hufs.ac.kr>
2025-05-05 21:30:57 +0900

    [feat] 채팅방 참여 기능 구현

--- src/main/java/org/example/chat/service/ChatService.java
+++ src/main/java/org/example/chat/service/ChatService.java
@@ -2,12 +2,8 @@ package org.example.chat.service;
 
 import org.example.chat.dto.request.ChatMessageRequest;
 import org.example.chat.dto.response.CreateChatRoomResponse;
-import org.example.chat.repository.entity.ChatRoomEntity;
-import org.example.users.repository.entity.UserEntity;
 
 public interface ChatService {
     void saveMessage(Long roomId, ChatMessageRequest request);
     CreateChatRoomResponse createChatRoom(Long productId, String chatRoomName);
-    void joinChatRoom(Long roomId);
-    void addParticipantToRoom(ChatRoomEntity chatRoom, UserEntity user);
 }
