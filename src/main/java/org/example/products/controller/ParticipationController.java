7878c2cb954237820837b2740b9f74b49c861772
rlarkdms1003 <rlarkdms0613@naver.com>
2025-05-07 14:38:22 +0900

    [feat] 공동 구매 참여 상태 확인 기능 구현

--- src/main/java/org/example/products/controller/ParticipationController.java
+++ src/main/java/org/example/products/controller/ParticipationController.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.example.common.ResponseEnum.SuccessResponseEnum;
 import org.example.common.repository.entity.CommonResponseEntity;
 import org.example.products.dto.request.GroupBuyingJoinRequest;
+import org.example.products.service.ParticipationService;
 import org.example.products.service.ProductService;
 import org.example.security.JwtTokenProvider;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/group-buyings")
@@ -17,6 +21,7 @@ public class ParticipationController {
 
     private final ProductService productService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final ParticipationService participationService;
 
     @PostMapping("/join")
     public ResponseEntity<?> joinGroupBuying(@RequestBody @Valid GroupBuyingJoinRequest request,
@@ -31,5 +36,25 @@ public class ParticipationController {
                         .response(SuccessResponseEnum.JOIN_SUCCESS)
                         .build()
         );
+    }
+
+    @GetMapping("/{group_buy_id}/status")
+    public ResponseEntity<?> checkParticipationStatus(@PathVariable("group_buy_id") Long postId,
+                                                      @RequestHeader("Authorization") String authorizationHeader) {
+        String token = authorizationHeader.replace("Bearer ", "");
+        Long userId = jwtTokenProvider.getUserId(token);
+
+        boolean isJoined = participationService.checkParticipationStatus(postId, userId);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("isJoined", isJoined);
+
+        return ResponseEntity.ok(
+                CommonResponseEntity.<Map<String, Object>>builder()
+                        .response(SuccessResponseEnum.JOIN_SUCCESS) // 또는 별도의 응답 메시지 enum 추가
+                        .data(response)
+                        .build()
+        );
+
     }
 }
