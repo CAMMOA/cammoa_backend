46d20bc42363786b77ffd381669b0ab8dbca911d
kim <202204266@hufs.ac.kr>
2025-05-12 15:56:57 +0900

    [feat] 채팅방 메시지 조회 기능 구현

--- src/main/java/org/example/security/config/SecurityConfig.java
+++ src/main/java/org/example/security/config/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -25,12 +30,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                //.cors(cors-> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
                 .httpBasic(AbstractHttpConfigurer::disable) //csrf 비활성화
                 .csrf(AbstractHttpConfigurer::disable) // HTTP Basic 비활성화
                 //JWT를 사용하기 때문에 세션 사용 X
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(a -> a.requestMatchers("/api/auth/signup", "/api/auth/login", "/connect").permitAll()
+                .authorizeHttpRequests(a -> a.requestMatchers("/api/auth/signup", "/api/auth/login", "/connect/**").permitAll()
                         .anyRequest().authenticated()) //특정 url패턴은 Authentication 객체 요구 X
                 //JWT 인증을 위하여 직접 구현한 필터 추가
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
@@ -45,10 +50,11 @@ public class SecurityConfig {
     }
 
     //연동을 위한 코드
-    /*@Bean
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트와 연동할 주소
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트와 연동할 주소
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*")); //모든 HTTP 메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*")); //모든 헤더값 허용
         configuration.setAllowCredentials(true); //자격 증명 허용
@@ -56,5 +62,5 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); //모든 url 패턴에 대해 cors 허용 설정
         return source;
-    }*/
+    }
 }
